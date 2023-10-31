8d2140b09dc08ac422f92133c38e3bec923dfd20
Yaser Nimer <yaser.nimer@ownbackup.com>
2023-10-31 23:47:36 +0200

    add functionality to get transactions table

--- pages/transactions_page.py
+++ pages/transactions_page.py
@@ -1,9 +1,15 @@
+import pandas as pd
+
 from commons.base_page import BasePage
 
 
 class TransactionsPage(BasePage):
     def __init__(self, *args):
         super().__init__(args[0])
+        self.transactions_table_css = 'table.table'
+        self.transactions_table_row_css = 'table.table tbody tr'
+        self.transactions_table_headers_css = 'table thead th'
+        self.transactions_table_row_cells_css = self.transactions_table_row_css + ':nth-child({}) td'
         self.logged_username_css = 'div.logged-user-name'
         self.completed_green_icon = 'span.status-pill.smaller.green'
 
@@ -13,5 +19,26 @@ class TransactionsPage(BasePage):
     def get_logged_username(self):
         return self.element_by_css(self.logged_username_css).text
 
+    def is_transactions_table_displayed(self):
+        return len(self.elements_by_css(self.transactions_table_css)) > 0
+
     def get_number_of_completed_transactions(self):
         return len(self.elements_by_css(self.completed_green_icon))
+
+    def get_transactions_table(self):
+        table_rows = self.elements_by_css(self.transactions_table_row_css)
+        table = {}
+        columns = [cell.text for cell in self.elements_by_css(self.transactions_table_headers_css)]
+
+        for row_index in range(1, len(table_rows) + 1):
+            cells = [cell.text for cell in
+                     self.elements_by_css(self.transactions_table_row_cells_css.format(row_index))]
+            row = {}
+
+            for i in range(len(columns)):
+                row[columns[i]] = cells[i]
+            table[row_index] = row
+
+        s = pd.DataFrame(table)
+        res_table = s.transpose()
+        return res_table
--- tests/test_login.py
+++ tests/test_login.py
@@ -11,6 +11,11 @@ class TestLogin:
         login_page.set_password("Passw212")
         login_page.click_login_btn()
         transactions_page = TransactionsPage(self.selenium_driver)
+        assert transactions_page.is_transactions_table_displayed(), "Error, transactions table is not displayed"
         assert transactions_page.is_logged_username_displayed(), "Error, Logged User is not displayed"
         assert transactions_page.get_logged_username() == 'Jack Gomez', "Error, Logged User is as expected"
-        assert transactions_page.get_number_of_completed_transactions() == 2, "Error, Number of completed transactions is incorrect"
+
+        transactions_page = transactions_page.get_transactions_table()
+        succeed_transactions = transactions_page[transactions_page['STATUS'] == 'Complete']
+        assert len(succeed_transactions) == 2, "Error, Number of completed transactions is incorrect"
+        # assert transactions_page.get_number_of_completed_transactions() == 2, "Error, Number of completed transactions is incorrect"
